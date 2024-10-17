673ab73bdb5e2bba243d34c4b07a79cf089b281d
VanessaKasei <kaseivanessa@gmail.com>
2024-10-17 17:59:24 +0300

    user model,route and controller

---
+++ controller/user.controler.js
@@ -0,0 +1,61 @@
+const User = require("../models/user.model");
+
+const registerUser = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new User({ email, password, role });
+    await newUser.save();
+
+    res.status(200).json({ message: "User registred successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) 
+  {
+    console.error(error);
+    res.status(500).json({message:"Server error", error})
+    
+  }
+};
+
+
+module.exports = {registerUser, loginUser}
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./route/user.route");
+// require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+
+mongoose
+  .connect(
+    "mongodb+srv://Vanessa:q2zmYzd3sffk@ecommerce.wnfkn.mongodb.net/Ecommerce?retryWrites=true&w=majority"
+)
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(5000, () => {
+      console.log("Server is listening on port 5000");
+    });
+  })
+  .catch((error) => {
+    console.error("Connection failed", error);
+  });
+
+
